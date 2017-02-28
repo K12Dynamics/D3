ce41b188e1f2267e68874e9e12065eb04938fa8b
Gary Clarke <gxclarke@gmail.com>
2017-02-28 17:37:26 -0600

    Prevent public access.

---
+++ docs/_static/authenticate.js
@@ -0,0 +1,29 @@
+(function (window) {
+
+    function getParameterByName(name, url) {
+        if (!url) {
+        url = window.location.href;
+        }
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
+
+    function getCookie(name) {
+        return document.cookie.replace(new RegExp("(?:(?:^|.*;\s*)" + name + "\s*\=\s*([^;]*).*$)|^.*$"), "$1");
+    }
+
+    //debugger;
+    var key = "authorized";
+
+    if (getParameterByName(key)) {
+        document.cookie = key + "=true";
+        window.location.href = "/";
+    } else if (!getCookie(key)) {
+        window.location.href = "/_static/access-denied.html";
+    }
+
+})(window);
